18c5e2150fc09bacb51672ec154a4cb3fbb8df5f
Sebastian Lopez Cruz <sebalopezx@gmail.com>
2024-05-02 20:56:06 -0300

    change deploy

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {"Content-Type" : "text/plain"});
-  res.end('Hello world!');
+  res.end('Hello world! JEJEJE');
 });
 
 const PORT = process.env.PORT || 4000;
